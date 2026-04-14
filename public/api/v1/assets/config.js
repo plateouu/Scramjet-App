1ce1b23744f3c8b50e2fb6533729b42608be4f3d
Administrator <daantaylor02@gmail.com>
2026-04-14 11:28:52 -0400

    fix: correct script loading order, implement standalone XOR, and force unregister old SW

--- public/api/v1/assets/config.js
+++ public/api/v1/assets/config.js
@@ -1,8 +1,17 @@
 self.__uv$config = {
     prefix: '/api/v1/net/',
     bare: '/bare/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
+    encodeUrl: (str) => {
+        if (!str) return str;
+        return encodeURIComponent(str.split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(''));
+    },
+    decodeUrl: (str) => {
+        if (!str) return str;
+        let [input, ...search] = str.split('?');
+        let decoded = decodeURIComponent(input);
+        let result = decoded.split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join('');
+        return result + (search.length ? '?' + search.join('?') : '');
+    },
     handler: '/api/v1/assets/handler.js',
     bundle: '/api/v1/assets/assets.js',
     config: '/api/v1/assets/config.js',
