e5279e8dade6d9afa1c8b98a7f6bf7005c435275
Administrator <daantaylor02@gmail.com>
2026-04-14 03:06:25 -0400

    fix: update internal sw.js path to use stealth assets

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-importScripts("/scram/scramjet.all.js");
+importScripts("/api/v1/assets/scramjet.all.js");
 
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
